48b8c52e517e802b97ceacd5fdfe9b79ae4b1b1c
Wisniewski, Luke <lawisnie@figtreesoftware.com>
2018-01-26 06:06:54 -0500

    - Update docker installation to install docker-compose

--- debian/init_docker.sh
+++ debian/init_docker.sh
@@ -22,3 +22,7 @@ sudo apt-get install -y docker-ce
 # Add the user to the docker group, or a permissions error may occur when calling docker commands
 sudo groupadd docker
 sudo usermod -a -G docker $USER
+
+# Install docker-compose
+sudo curl -L https://github.com/docker/compose/releases/download/1.18.0/docker-compose-`uname -s`-`uname -m` -o /usr/local/bin/docker-compose
+sudo chmod +x /usr/local/bin/docker-compose
