60dccc6432090ac367a8c7028ee658b7c31e495b
Wisniewski, Luke <lawisnie@figtreesoftware.com>
2019-06-04 21:35:16 -0400

    - Update manual debian install steps

--- debian/manualSteps.sh
+++ debian/manualSteps.sh
@@ -1,3 +1,4 @@
+# Install debian
 su -
 <enter password>
 # Sudo isn't installed by default, so we need to install it
@@ -6,5 +7,10 @@ apt install -y git
 su - lawisnie
 # Add lawisnie to the sudoer's group
 sudo usermod -aG sudo,adm lawisnie
+# Create an ssh keypair
+ssh-keygen -t rsa -C "lawisnie@figtreesoftware.com"
+# Add the ssh public key to azure devops (open in vi and use mouse to select text)
+vi /home/lawisnie/.ssh/id_rsa.pub
 # Clone the repo
-git clone https://figtreesoftware.visualstudio.com/DefaultCollection/Base/_git/DevOps_Unix
+git clone git@ssh.dev.azure.com:v3/figtreesoftware/Base/DevOps_Unix
+# Cd to any desired directory and run docker-compose up -d
