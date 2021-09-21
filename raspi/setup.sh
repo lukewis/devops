2c1f76de96a0db6e04a04609f3f7d39a18f24d01
Luke Wisniewski <lawisnie@figtreesoftware.com>
2021-09-21 05:51:17 -0400

    - Finished initial version of setup.sh

--- raspi/setup.sh
+++ raspi/setup.sh
@@ -1,10 +1,26 @@
 echo "Reading latest updates"
-sudo apt-get update -y
-sudo apt-get upgrade -y
+sudo apt update -y
+sudo apt upgrade -y
+
+echo "Adding ability to run a script after boot"
+sudo cp runonce.service /etc/systemd/system
+sudo systemctl enable runonce.service
 
 echo "Installing docker"
 curl -fsSL https://get.docker.com | sh -
 sudo usermod -aG docker $USER
 
-echo "Installing K3S"
-curl -sfL https://get.k3s.io | sh -
+echo "Installing docker-compose"
+sudo apt install libffi-dev libssl-dev
+sudo apt install python3-dev
+sudo apt install -y python3 python3-pip
+sudo pip3 install docker-compose
+
+echo "Preparing to install K3S"
+cp daemon.json /etc/docker/
+sudo sed -i '$ s/$/ cgroup_enable=cpuset cgroup_enable=memory cgroup_memory=1 swapaccount=1/' /boot/cmdline.txt
+echo "System will now reboot..."
+sudo cp setup-part2.sh /var/local/runonce.sh
+sudo shutdown -r now
+
+# Script continues in setup-part2.sh...
