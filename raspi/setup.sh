fd463ce473bc5b665a2386dea9a7313023a8d0a9
Luke Wisniewski <lawisnie@figtreesoftware.com>
2021-09-21 05:59:11 -0400

    - Fixed prompt when installing docker-compose

--- raspi/setup.sh
+++ raspi/setup.sh
@@ -11,8 +11,8 @@ curl -fsSL https://get.docker.com | sh -
 sudo usermod -aG docker $USER
 
 echo "Installing docker-compose"
-sudo apt install libffi-dev libssl-dev
-sudo apt install python3-dev
+sudo apt install -y libffi-dev libssl-dev
+sudo apt install -y python3-dev
 sudo apt install -y python3 python3-pip
 sudo pip3 install docker-compose
 
