4d8af3a6b9a52c011cb1a5e3c767c7c756e5f5d5
Luke Wisneiwski <luke.wisniewski@gmail.com>
2021-09-22 12:54:20 +0100

    Added echo to setup.sh

--- raspi/setup.sh
+++ raspi/setup.sh
@@ -15,5 +15,6 @@ sudo pip3 install docker-compose
 echo "Preparing to install K3S"
 cp daemon.json /etc/docker/
 sudo sed -i '$ s/$/ cgroup_enable=cpuset cgroup_enable=memory cgroup_memory=1 swapaccount=1/' /boot/cmdline.txt
+echo "Setup part 1 is complete.  If there were no errors, please reboot the pi (sudo shutdown -r now) and run setup-part2.sh"
 
 # Script continues in setup-part2.sh...
