29f1d8e2de296ab771cf5b2f8bad2c55000a6860
Luke Wisniewski <luke.wisniewski@gmail.com>
2021-09-22 12:00:29 +0000

    Updated setup-part2.sh

--- raspi/setup-part2.sh
+++ raspi/setup-part2.sh
@@ -1,6 +1,6 @@
 echo "Installing K3s"
 PRIMARY_NODE=192.168.4.70
-
+echo "Acquiring node token from the primary node: $PRIMARY_NODE"
 scp pi@$PRIMARY_NODE:~/node-token ~/node-token
 source ~/node-token
 curl -sfL https://get.k3s.io K3S_URL=https://$PRIMARY_NODE:6443 K3S_TOKEN=$K3S_TOKEN | sh -
