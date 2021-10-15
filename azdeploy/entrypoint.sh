63f2edbf663559ec9e760b26b29728c7600ecccf
Luke Wisniewski <lukewis@users.noreply.github.com>
2021-10-14 22:31:56 -0400

    - Add shebang

--- azdeploy/entrypoint.sh
+++ azdeploy/entrypoint.sh
@@ -1,1 +1,3 @@
+#!/bin/sh
+
 ./config.sh --deploymentgroup --deploymentgroupname "Default" --unattended --acceptteeeula --agent $HOSTNAME --url https://dev.azure.com/figtreesoftware/ --work _work --projectname 'Base' --addDeploymentGroupTags --deploymentGroupTags "k8s" --auth PAT --token $AZPAT; ./run.sh;
