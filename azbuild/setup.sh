2cb4a51cc9f2a609b18ec4f301f79f43636ed267
Luke Wisniewski <lukewis@users.noreply.github.com>
2021-10-16 07:22:58 -0400

    - Use "docker-in-docker" script instead of actually trying to install docker

--- azbuild/setup.sh
+++ azbuild/setup.sh
@@ -1,15 +1,13 @@
 echo "Installing docker"
-curl -fsSL https://download.docker.com/linux/debian/gpg | gpg --dearmor -o /usr/share/keyrings/docker-archive-keyring.gpg
-echo \
-    "deb [arch=$(dpkg --print-architecture) signed-by=/usr/share/keyrings/docker-archive-keyring.gpg] https://download.docker.com/linux/debian \
-    $(lsb_release -cs) stable" | sudo tee /etc/apt/sources.list.d/docker.list > /dev/null \
-apt update
-apt install -y docker-ce docker-ce-cli containerd.io
+curl -fkSL -o docker-debian.sh https://raw.githubusercontent.com/microsoft/vscode-dev-containers/main/script-library/docker-debian.sh
+chmod 755 docker-debian.sh
+./docker-debian.sh
 
 echo "Installing kubectl"
 curl -LO "https://dl.k8s.io/release/$(curl -L -s https://dl.k8s.io/release/stable.txt)/bin/linux/amd64/kubectl"
 install -o root -g root -m 0755 kubectl /usr/local/bin/kubectl
 
+echo "Installing azure pipeline agent"
 curl -fkSL -o vstsagent.tar.gz https://vstsagentpackage.azureedge.net/agent/2.193.0/vsts-agent-linux-x64-2.193.0.tar.gz;
 tar -zxvf vstsagent.tar.gz; 
 ./bin/installdependencies.sh
