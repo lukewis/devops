cee0eb8d906c3cd646a1859d044044ab92b9f653
Luke Wisniewski <lukewis@users.noreply.github.com>
2021-10-16 07:12:52 -0400

    - Add docker to the base image

--- azbuild/setup.sh
+++ azbuild/setup.sh
@@ -1,3 +1,11 @@
+echo "Installing docker"
+curl -fsSL https://download.docker.com/linux/debian/gpg | gpg --dearmor -o /usr/share/keyrings/docker-archive-keyring.gpg
+echo \
+    "deb [arch=$(dpkg --print-architecture) signed-by=/usr/share/keyrings/docker-archive-keyring.gpg] https://download.docker.com/linux/debian \
+    $(lsb_release -cs) stable" | sudo tee /etc/apt/sources.list.d/docker.list > /dev/null \
+apt update
+apt install -y docker-ce docker-ce-cli containerd.io
+
 echo "Installing kubectl"
 curl -LO "https://dl.k8s.io/release/$(curl -L -s https://dl.k8s.io/release/stable.txt)/bin/linux/amd64/kubectl"
 install -o root -g root -m 0755 kubectl /usr/local/bin/kubectl
